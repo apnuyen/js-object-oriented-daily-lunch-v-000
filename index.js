ca64da8e0bd24813382b7e57bbd96b9379a18567
An Nuyen <annuyen@gmail.com>
2018-02-01 17:17:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+let customerId = 0
+
+class Customer {
+  constructor(name, employer){
+    this.id = ++customerId 
+    
+  }
+}
+
+class Meal {
+  
+}
+
+class Delivery {
+  
+}
+
+class Employer {
+  
+}
